fc9df03d974eac63f7577d6cde97064b6e551cdc
0xB370 <mansilla_alberto@outlook.com>
2023-05-24 21:53:30 -0300

    Local Storage Management

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -12,6 +12,8 @@ const unsplashAccessKey = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY;
 function Game() {
   const router = useRouter();
   const category = useSearchParams().get('category') || 'nature';
+  const [fullGameState, setFullGameState] = useState(useSelector((state) => state.gameState));
+  const [fullHighestScores, setFullHighestScores] = useState(useSelector((state) => state.updateScore));
   const [images, setImages] = useState(useSelector((state) => state.gameState[category].images));
   const [flippedCards, setFlippedCards] = useState(useSelector((state) => state.gameState[category].flippedCards));
   const [matchedCards, setMatchedCards] = useState(useSelector((state) => state.gameState[category].matchedCards));
@@ -31,6 +33,22 @@ function Game() {
     return 0; 
   }));
 
+  const storeSessionData = () => {
+    const storedSessionData = localStorage.getItem('gameState');
+    if (storedSessionData) {
+      const parsedSessionData = JSON.parse(storedSessionData);
+      localStorage.setItem('gameState', JSON.stringify({
+        ...parsedSessionData,
+        [category]: gameState
+      }));
+    } else {
+      localStorage.setItem('gameState', JSON.stringify({
+        ...fullGameState,
+        [category]: gameState
+      }));
+    }
+  }
+
   const updateHighestScore = (score) => {
     if (category === 'nature') {
       dispatch(updateNatureScore(score));
@@ -39,6 +57,7 @@ function Game() {
     } else if (category === 'food') {
       dispatch(updateFoodScore(score));
     }
+    localStorage.setItem('highestScores', JSON.stringify(fullHighestScores));
   };
 
   const shuffleArray = (array) => {
@@ -51,28 +70,80 @@ function Game() {
   };
 
   useEffect(() => {
-    if (retryCount === 0) {
-      const fetchImages = async () => {
-        try {
-          const response = await fetch(
-            `https://api.unsplash.com/photos/random?count=15&query=${category}&client_id=${unsplashAccessKey}`
-          );
-          const data = await response.json();
-          const imageUrls = data.map((image) => image.urls.regular);
-          // Ordered images to test
-          const imagesArr = imageUrls.flatMap((element) => [element, element]);
-          // const imagesArr = shuffleArray(imageUrls.flatMap((element) => [element, element]));
-          setImages(imagesArr);
-          dispatch(updateImages(category, imagesArr ));
-        } catch (error) {
-          console.error('Error fetching images:', error);
-        }
-      };
+    const fetchImages = async () => {
+      try {
+        const response = await fetch(
+          `https://api.unsplash.com/photos/random?count=15&query=${category}&client_id=${unsplashAccessKey}`
+        );
+        const data = await response.json();
+        const imageUrls = data.map((image) => image.urls.regular);
+        // Ordered images to test
+        const imagesArr = imageUrls.flatMap((element) => [element, element]);
+        // const imagesArr = shuffleArray(imageUrls.flatMap((element) => [element, element]));
+        setImages(imagesArr);
+        dispatch(updateImages(category, imagesArr ));
+      } catch (error) {
+        console.error('Error fetching images:', error);
+      }
+    };
+    // Retrieve the session data from Local Storage
+    const sessionData = localStorage.getItem('gameState');
+    console.log("storedSessionData");
+    console.log(sessionData);
+    if (sessionData) {
+      const parsedSessionData = JSON.parse(sessionData);
+      console.log('parsedSessionData');
+      console.log(parsedSessionData);
+      
+      
+      if (parsedSessionData[category].images.length > 0) {
+        setFlippedCards(parsedSessionData[category].flippedCards);
+        setMatchedCards(parsedSessionData[category].matchedCards);
+        setRetryCount(parsedSessionData[category].retryCount);
+        setScore(parsedSessionData[category].score);
+        setIsSessionEnded(parsedSessionData[category].isSessionEnded);
+        setImages(parsedSessionData[category].images);
+        dispatch(updateImages(category, parsedSessionData[category].images ));
+      } else {
+        fetchImages();
+      }
+    }else {
       fetchImages();
     }
+    dispatch(updateGameState(category, {
+      flippedCards,
+      matchedCards,
+      retryCount,
+      score,
+      isSessionEnded,
+    }));
+    const storedSessionData = localStorage.getItem('gameState');
+    if (storedSessionData) {
+      const parsedSessionData = JSON.parse(storedSessionData);
+      localStorage.setItem('gameState', JSON.stringify({
+        ...parsedSessionData,
+        [category]: gameState
+      }));
+    } else {
+      localStorage.setItem('gameState', JSON.stringify({
+        ...fullGameState,
+        [category]: gameState
+      }));
+    }
+    // fetchImages();
   }, [category]);
 
   useEffect(() => {
+    // Retrieve the full game state from Local Storage
+    const storedGameState = localStorage.getItem('gameState');
+    if (storedGameState) {
+      const parsedGameState = JSON.parse(storedGameState);
+      setFullGameState(parsedGameState);
+    }
+    console.log('flippedCards');
+    console.log(flippedCards);
+    
+    
     if (flippedCards.length === 2) {
       const [card1, card2] = flippedCards;
       if (images[card1] === images[card2]) {
@@ -85,15 +156,33 @@ function Game() {
       }
       setRetryCount((prevRetryCount) => prevRetryCount + 1);
     }
-  }, [flippedCards]);
+
+    dispatch(updateGameState(category, {
+      flippedCards,
+      matchedCards,
+      retryCount,
+      score,
+      isSessionEnded,
+    }));
+    localStorage.setItem('gameState', JSON.stringify({
+      ...fullGameState,
+      [category]: {
+        ...fullGameState[category],
+          flippedCards,
+          matchedCards,
+          retryCount,
+          score,
+          isSessionEnded,
+      }
+    }));
+
+  }, [flippedCards, images]);
 
   const handleClick = (index) => {
     if (flippedCards.length === 2 || flippedCards.includes(index)) {
       return;
     }
-
     setFlippedCards((prevFlippedCards) => [...prevFlippedCards, index]);
-
     if (flippedCards.length === 1) {
       setRetryCount((prevRetryCount) => prevRetryCount + 1);
       if (images[flippedCards[0]] === images[index]) {
@@ -111,13 +200,48 @@ function Game() {
           }
           // Dispatch action to save the state in Redux
           dispatch(resetGameState(category));
+          storeSessionData();
         }        
       } else {
         setTimeout(() => {
           setFlippedCards([]);
         }, 1000);
       }
+      console.log('retryCount desde handleClick');
+      console.log(retryCount);
+      
+      
+      dispatch(updateGameState(category, {
+        flippedCards,
+        matchedCards,
+        retryCount,
+        score,
+        isSessionEnded,
+      }));
     }
+    dispatch(updateGameState(category, {
+      flippedCards,
+      matchedCards,
+      retryCount,
+      score,
+      isSessionEnded,
+    }));
+    /*
+    console.log('gameState desde handleClick');
+    console.log(gameState);
+    localStorage.setItem('gameState', JSON.stringify({
+      ...fullGameState,
+      [category]: {
+        ...fullGameState[category],
+          flippedCards,
+          matchedCards,
+          retryCount,
+          score,
+          isSessionEnded,
+      }
+    }));
+    */
+    storeSessionData();
   };
 
   const handleBackClick = () => {
@@ -127,7 +251,8 @@ function Game() {
       retryCount,
       score,
       isSessionEnded,
-    }))
+    }));
+    storeSessionData();
   };
 
   const renderGrid = () => {
